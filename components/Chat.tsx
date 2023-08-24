c0be049bcf09e5278be64259bdaf0614d2d03d0f
li.li <mark.li210812@gmail.com>
2023-08-24 23:20:27 +0800

    style: fix eslint

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,18 +1,18 @@
 'use client'
 
 import { useChat } from 'ai/react'
-import Message from './Message.tsx'
-import { Input } from './ui/input.tsx'
+import Message from './Message'
+import { Input } from './ui/input'
 import { Send } from 'lucide-react'
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: process.env.NODE_ENV === 'development' ? '/api/chat-dev' : '/api/chat'
+    api: process.env.NODE_ENV === 'development' ? '/api/chat-dev' : '/api/chat',
   })
 
   return (
     <div className="w-full flex-1 relative">
-      {messages.map(m => (
+      {messages.map((m) => (
         <div className="flex items-start gap-4 mb-4" key={m.id}>
           <Message m={m} />
         </div>
