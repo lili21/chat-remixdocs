c9546e11db2156964dbcfb2fd3307ebec1c0870c
li.li <mark.li210812@gmail.com>
2023-08-26 17:05:51 +0800

    feat: add source code link

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Chat with Remix Docs
 
+Ask Question to Remix Docs, powder by OpenAI GPT 3.5 and Supabase.
 
+![showcase](https://media.discordapp.net/attachments/1144689593841614918/1144727015853924464/CleanShot_2023-08-25_at_13.16.02.png?width=1138&height=986)
+
+
+## Credits
+
+- [nextjs-openai-doc-search](https://github.com/supabase-community/nextjs-openai-doc-search). 
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,10 @@ import '@/styles/globals.css'
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-const inter = Inter({ subsets: ['latin'] })
 import Chat from '@/components/Chat'
+import { Github } from 'lucide-react'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const api = process.env.NODE_ENV === 'development' ? '/api/chat-dev' : '/api/chat'
@@ -17,6 +19,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <a
+          className="fixed top-6 right-6"
+          href="https://github.com/lili21/chat-remixdocs"
+          target="_blank"
+        >
+          <Github size="30" stroke="#4169E1" />
+        </a>
         <Chat api={api} />
       </main>
     </>
