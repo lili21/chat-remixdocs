b0e5def48b4d61b1173fb86c30652c4f305d3b5a
li.li <mark.li210812@gmail.com>
2023-08-28 23:13:08 +0800

    fix: metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,28 @@
 import '@/styles/globals.css'
 
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Chat from '@/components/Chat'
 import { Github } from 'lucide-react'
+import { Metadata } from 'next'
 
-const inter = Inter({ subsets: ['latin'] })
+export const metadata: Metadata = {
+  title: 'Chat With Remix Docs',
+  description: 'Ask question about Remix',
+  viewport: 'width=device-width, initial-scale=1',
+}
 
 export default function Home() {
   const api = process.env.NODE_ENV === 'development' ? '/api/chat-dev' : '/api/chat'
   return (
-    <>
-      <Head>
-        <title>Chat with Remix docs</title>
-        <meta name="description" content="chat with remix docs" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <a
-          className="fixed top-6 right-6"
-          href="https://github.com/lili21/chat-remixdocs"
-          target="_blank"
-        >
-          <Github size="30" stroke="#4169E1" />
-        </a>
-        <Chat api={api} />
-      </main>
-    </>
+    <main className={styles.main}>
+      <a
+        className="fixed top-6 right-6"
+        href="https://github.com/lili21/chat-remixdocs"
+        target="_blank"
+      >
+        <Github size="30" stroke="#4169E1" />
+      </a>
+      <Chat api={api} />
+    </main>
   )
 }
